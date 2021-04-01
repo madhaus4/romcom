f4bd95e4056e108b19f14262473e5066cb781b81
Maria DelSignore <maria.ad27@gmail.com>
2021-03-31 20:11:23 -0600

    Complete functionality iteration 0

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 // Create variables targetting the relevant DOM elements here 👇
 
+// need random image - data file holds var covers
+// cover file holds cover class with cover property and value coverImgSrc
+// need to manipulate '.cover-image' in index.html to random
+
+
 // create a variable targeting the class '.cover-image'
+var coverImage = document.querySelector('.cover-image');
+
 // create a variable targeting the class '.cover-title'
+var coverTitle = document.querySelector('.cover-title');
 // create a variable targeting the 'h3' tag
+var tagline1 = document.querySelector('.tagline-1');
+var tagline2 = document.querySelector('.tagline-2');
+
+// use innerText to change html value
+
 
 // We've provided a few variables below
 var savedCovers = [
@@ -13,12 +26,28 @@ var currentCover;
 // Add your event listeners here 👇
 
 // add eventListener to of window.eventListener('load', functionName)
+window.addEventListener('load', getRandomCover)
 
 // Create your event handlers and other functions here 👇
-
 
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
-// create randomRomComCover function. then add this function to the window load event listener. 
+// create randomRomComCover function. then add this function to the window load event listener.
+function getRandomCover() {
+  var randomCover = covers[getRandomIndex(covers)]
+  var randomTitle = titles[getRandomIndex(titles)]
+  var randomTagline1 = descriptors[getRandomIndex(descriptors)]
+  var randomTagline2 = descriptors[getRandomIndex(descriptors)];
+  coverImage.src = randomCover;
+  coverTitle.innerText = randomTitle;
+  tagline1.innerText = randomTagline1;
+  tagline2.innerText = randomTagline2;
+
+currentCover = new Cover(randomCover, randomTitle, randomTagline1, randomTagline2);
+
+
+}
+
+console.log(getRandomIndex);
